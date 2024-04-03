3011f7be23b3fb33b0d61b9c7f006e0fcff1bccc
donghun0508 <alswn4516@gmail.com>
2024-04-03 11:23:45 +0900

    first commit

---
+++ src/main/java/com/example/ecommerce/admin/acl/service/ECommerceAdminAclServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.admin.acl.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAdminAclServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceAdminAclServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommerce/admin/acl/service/ECommerceAdminAclServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.admin.acl.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAdminAclServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
