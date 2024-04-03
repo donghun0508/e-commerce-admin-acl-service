20bca9718f2f84775525129c886fd59bd1581926
donghun0508 <alswn4516@gmail.com>
2024-04-03 11:50:53 +0900

    feature: Main 메서드 생성

--- src/main/java/com/example/ecommerce/admin/acl/service/ECommerceAdminAclServiceApplication.java
+++ src/main/java/com/example/ecommerce/admin/acl/service/ECommerceAdminAclServiceApplication.java
@@ -8,6 +8,7 @@ public class ECommerceAdminAclServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ECommerceAdminAclServiceApplication.class, args);
+
     }
 
 }
